e3cc8b729f80c527fc8645cd8517b9b183e0b7fb
Mark Bell <me@markb.co.uk>
2016-12-14 14:28:55 +0000

    Add tests for MapToConstructor method

---
+++ MAB.SimpleMapper.Test/ModelConstructorOnly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MAB.SimpleMapper.Test
+{
+    public class ModelConstructorOnly
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+    }
+}
